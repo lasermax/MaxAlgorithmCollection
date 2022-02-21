f58d031fd88af9002118db0b9711343044a0f4d8
Max Seeliger <maximilian.seeliger@gmail.com>
2022-02-22 00:03:00 +0100

    Added Tests to fully cover BaseStack.java Added Tests to fully cover relevant parts of ResizeableStack.java

--- test/stack/BaseStackTest.java
+++ test/stack/BaseStackTest.java
@@ -29,4 +29,69 @@ class BaseStackTest {
         assertEquals(2, stack.size());
     }
 
+    @Test
+    @DisplayName("isEmpty returns correct result after removing all items")
+    void isEmptyReturnsCorrectResultAfterRemovingAllItems() {
+        BaseStack<String> stack = new BaseStack<>(new String[]{"Test", "Test2"});
+        assertEquals("Test2", stack.pop());
+        assertFalse(stack.isEmpty());
+        assertEquals("Test", stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    @DisplayName("size returns correct results")
+    void sizeReturnsCorrectResults() {
+        BaseStack<Integer> stack = new BaseStack<>(10, 5, 6);
+
+        assertEquals(2, stack.size());
+        stack.pop();
+        assertEquals(1, stack.size());
+        stack.pop();
+        assertEquals(0, stack.size());
+        stack.push(5);
+        assertEquals(1, stack.size());
+    }
+
+    @Test
+    @DisplayName("correct STD_SIZE with no parameter constructor")
+    void correctStdSizeWithNoParameterConstructor() {
+        BaseStack<Character> stack = new BaseStack<>();
+
+        for (int i = 0; i < 10; i++) {
+            stack.push((char)i);
+        }
+
+        assertThrows(IllegalStateException.class, ()->stack.push('h'), "Attempted call to push() on full Stack");
+    }
+
+    @Test
+    @DisplayName("throws error if size parameter is smaller then number of initial elements")
+    void throwsErrorIfSizeParameterIsSmallerThenNumberOfInitialElements() {
+        // equals should not throw error
+        BaseStack<Integer> stack = new BaseStack<>(4, 5, 8, 6, 8);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            BaseStack<Character> stack1 = new BaseStack<>(2, 'a', 'b', 'c');
+        }, "Size can not be smaller then the number of initial elements");
+
+    }
+
+    @Test
+    @DisplayName("throws error for call on pop to empty stack")
+    void throwsErrorForCallOnPopToEmptyStack() {
+        BaseStack<Object> stack = new BaseStack<>();
+
+        assertThrows(IllegalStateException.class, stack::pop, "Attempted call to pop() on empty stack");
+    }
+
+    @Test
+    @DisplayName("stack size must be not negative")
+    void stackSizeMustBeNotNegative() {
+        //zero is a valid size
+        BaseStack<Object> stack = new BaseStack<>(0);
+
+
+        assertThrows(IllegalArgumentException.class, () -> new BaseStack<>(-3), "Stack size must be a non-negative Integer");
+    }
 }
--- test/stack/ResizeableStackTest.java
+++ test/stack/ResizeableStackTest.java
@@ -1,7 +1,31 @@
 package stack;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResizeableStackTest {
+    @Test
+    @DisplayName("throws error with a size smaller than the current number of elements")
+    void throwsErrorWithASizeSmallerThanTheCurrentNumberOfElements() {
+        ResizeableStack<String> stack = new ResizeableStack<>(3, "Test1", "Test2", "Test3");
+        assertThrows(IllegalArgumentException.class, () -> stack.resize(2), "Size can not be smaller then the number of elements on the stack");
 
+        assertThrows(IllegalArgumentException.class, () -> stack.resize(-5), "Size can not be smaller then the number of elements on the stack");
+
+        stack.pop();
+        stack.resize(2);
+    }
+
+    @Test
+    @DisplayName("correct size change")
+    void correctSizeChange() {
+        ResizeableStack<String> stack = new ResizeableStack<>(3, "Test1", "Test2", "Test3");
+
+        assertEquals(3, stack.stack.length);
+        stack.resize(15);
+        assertEquals(15, stack.stack.length);
+
+    }
 }
